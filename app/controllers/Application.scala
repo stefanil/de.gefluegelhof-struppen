c404d8762c84c20a4ba69af589aafc615f524534
Stefan Illgen <stefan.illgen.ebenheit@gmail.com>
2014-01-03 00:42:50 +0100

    Initial markup, images, views, ...

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,38 @@
 package controllers
 
+import java.io.File
 import play.api._
 import play.api.mvc._
+import play.api.templates.Html
+import java.net.URI
+import java.net.URL
+import java.io.FileReader
+import java.util.ArrayList
+import scala.collection.mutable.MutableList 
 
+/**
+ * @author stefan.illgen
+ */
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect("/unternehmen")
+  }
+
+  def angebot = Action {
+    Ok(views.html.angebot.render)
+  }
+
+  def oeffnungszeiten = Action {
+    Ok(views.html.oeffnungszeiten.render)
+  }
+
+  def kontakt = Action {
+    Ok(views.html.kontakt.render)
+  }
+  
+  def unternehmen = Action {
+    Ok(views.html.unternehmen.render)
   }
 
 }
---
+++ public/unternehmen/markdown/me.md
@@ -0,0 +1,16 @@
+%%% markdown_metadata_plugin 
+creator=stefan
+createdOn=1387770382
+href=@routes.Assets.at("me/images/me16.png")
+img=@routes.Assets.at("me/images/me16.png")
+%%%
+#Me
+This is an online presentation for presenting my skills.
+
+##Technical Features
+
+###Built on Play Framework v2.2.1
+* dependency and build management based on SBT
+* built using Scala programming language in frontend and backend
+
+![Alternativtext](Bild-URL "Bildtitel hier")
